302453335f8835644bd21e73d88335cfb8a1b95f
Artem <temapro04@gmai.com>
2023-06-19 17:37:32 +0300

    Implementation Init and RandomInit

--- Percolation.cs
+++ Percolation.cs
@@ -8,11 +8,50 @@ namespace percolation
         private byte[] percolationArr = new byte[0];
         private bool[] openedArr = new bool[0]; // true/false array
         private byte[] sizeArr = new byte[0];
+        private byte topSite = 2;
+        private int size = 0;
+        private int openSites = 0;
 
         // creates n-by-n grid, with all sites initially blocked
         public void Init(int n)
         {
+            size = n;
+            percolationArr = new byte[size * size + 2];
+            sizeArr = new byte[size * size + 2];
+            openedArr = new bool[size * size + 2];
+            for (byte i = 0; i < size * size + 2; i++)
+            {
+                percolationArr[i] = i;
+                sizeArr[i] = 1;
+            }
+            percolationArr[0] = topSite;
+        }
 
+        public void RandomInit(int n)
+        {
+            Init(n);
+            for (int i = 1; i < size * size + 1; i++)
+            {
+                Random random = new Random();
+                int row;
+                int col;
+                int j = random.Next(0, 2);
+
+                if (i % size == 0)
+                {
+                    row = i / size;
+                    col = size; 
+                } else
+                {
+                    row = i / size + 1;
+                    col = i % size;
+                }
+
+                if (j == 1)
+                {
+                    Open(row, col);
+                }
+            }
         }
 
         // opens the site (row, col) if it is not open already
